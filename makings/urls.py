fa3c35169c5c1385c194b4cd5815ffe7dbe4ae51
AlaminSarkerFRII <alamin.education1997@gmail.com>
2023-09-23 12:20:19 +0600

    setup templates of djngo projects

---
+++ makings/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.urls import path,include
+from core.views import index
+
+urlpatterns = [
+    path('',include('core.urls')),
+    path('admin/', admin.site.urls),
+]
